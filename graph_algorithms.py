c4095ddc6935964bca45a54bbafa9499b5985756
unknown <samwondim@gmail.com>
2023-06-26 13:10:07 +0300

    added code to the course under graph algorithms. dfs, bfs, graph representation

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -1,15 +1,72 @@
 class Graph:
-    def __init__(self):
-        self.adj_list = []
+    def __init__(self, num_nodes, edges):
+        self.data = [[] for _ in range(num_nodes)]
+        for v1, v2 in edges:
+            self.data[v1].append(v2)
+            self.data[v2].append(v1)
+            
+    def __repr__(self):
+        return "\n".join(["{} : {}".format(i, neighbors) for (i, neighbors) in enumerate(self.data)])
+
+    def __str__(self):
+        return repr(self)
+
+def bfs(graph, source):
+    visited = [False] * len(graph.data)
+    queue = []
+    
+    visited[source] = True    
+    queue.append(source)
+    i = 0
+    
+    while i < len(queue):
+        for v in graph.data[queue[i]]:
+            if not visited[v]:
+                visited[v] = True
+                queue.append(v)
+        i += 1
         
-    def add_edge(self, n_list):
-        self.adj_list.append(n_list)
-        
-    def remove_edge(self, edge):
-        for l in adj_list:
-            for i in l:
-                if i == edge:
-                    l.remove(i)
-                    
-    def get_list(self):
-        return self.adj_list
+    return queue
+
+def dfs(graph, source):
+    visited = [False] * len(graph.data)
+    stack = [source]
+    result = []
+
+    while len(stack) > 0:
+        current = stack.pop()
+        if not visited[current]:
+            result.append(current)
+            visited[current] = True
+            for v in graph.data[current]:
+                stack.append(v)
+
+    return result
+
+class DWGraph:
+    def __init__(self, num_nodes, edges, directed=False):
+        self.data = [[] for _ in range(num_nodes)]
+        self.weight = [[] for _ in range(num_nodes)]
+
+        self.directed = directed
+        self.weighted = len(edges) > 0 and len(edges[0]) == 3
+
+        for e in edges:
+            self.data[e[0]].append(e[1])
+            if self.weighted:
+                self.weight[e[0]].append(e[2])
+
+            if not directed:
+                self.data[e[1]].append(e[0])
+                if self.weighted:
+                    self.data[e[1]].append(e[2])
+
+    def __repr__(self):
+        result = ""
+        for i in range(len(self.data)):
+            pairs = list(zip(self.data[i], self.weight[i]))
+            result += "{}: {}\n".format(i, pairs)
+        return result
+
+    def __str__(self):
+        return repr(self)
